4fd820fc08eb133fb00afc24c2d31233320e5d30
duc do <tvietduc122@gmail.com>
2022-10-22 15:15:12 +0700

    register database

---
+++ Controllers/AuthController.js
@@ -0,0 +1,23 @@
+const userModel = require('../Models/UserModel');
+
+const register = async (req, res) => {
+    try {
+        // get infor from client
+        const { username, password, email } = req.body;
+        // create data to database
+        await userModel.create({
+            username: username,
+            password: password,
+            email: email,
+            role: 'regular',
+        });
+        return res.status(200).send('register user');
+    } catch (error) {
+        console.log('error', error);
+    }
+    
+};
+
+module.exports = {
+    register: register,
+};
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 const cors = require('cors');
 
 const userRoute = require('./Router/UserRoute');
+const authRoute = require('./Router/AuthRoute');
 const connectDb = require('./Services/ConnectDbService');
 
 const port = 5000;
 // middleware apply cors add all request
 app.use(cors());
+// middleware get info from client by req.body
+app.use(express.json());
 
 
 // connent database
@@ -15,6 +18,7 @@ connectDb();
 
 // middleware router
 app.use('/users', userRoute);
+app.use('/api/auth', authRoute);
 
 
 app.listen(port, function(){
