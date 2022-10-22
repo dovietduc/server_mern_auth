a7ba33b80d8ff1b91d97c5e91123af3424257bc8
duc do <tvietduc122@gmail.com>
2022-10-22 07:54:51 +0700

    create schema

---
+++ Models/UserModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    role: String
+});
