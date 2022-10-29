5291e370cc6488a38d241f43296ebf7b0e47bfc6
duc do <tvietduc122@gmail.com>
2022-10-29 08:32:04 +0700

    jwt valide user

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,14 +1,21 @@
 const jwt = require('jsonwebtoken');
-const getListUser = (req, res) => {
+const userModel = require('../Models/UserModel');
+const getListUser = async (req, res) => {
     // 1. get token from client
     const bearerHeader = req.headers['authorization'];
     const accessToken = bearerHeader.split(' ')[1];
     
-    // 2. verifile token
-    const decodeJwt = jwt.verify(accessToken, process.env.SECRET_JWT);
-    console.log(decodeJwt);
-
-   
+    try {
+        // 2. verifile token
+        const decodeJwt = jwt.verify(accessToken, process.env.SECRET_JWT);
+        if(decodeJwt) {
+            const users = await userModel.find();
+            res.status(200).send(users);
+        }
+    } catch (error) {
+        // gui ma loi client de client biet refresh token
+        console.log(error);
+    }
     
 }
 
